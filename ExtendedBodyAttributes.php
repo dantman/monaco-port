c83390d217c5310c9ebbe375f2b1a46451523273
Daniel Friesen <pub-github@nadir-seen-fire.com>
2010-10-10 11:39:27 -0700

    Add the code you can use to add the extra loggedout and mainpage body classes.

---
+++ ExtendedBodyAttributes.php
@@ -0,0 +1,15 @@
+<?php
+
+$wgHooks['OutputPageBodyAttributes'][] = 'egExtendedOutputPageBodyAttributes';
+function egExtendedOutputPageBodyAttributes( $out, $sk, &$bodyAttrs ) {
+	global $wgUser;
+	
+	if ( !$wgUser->isLoggedIn() )
+		$bodyAttrs['class'] .= ' loggedout';
+	
+	if ( $out->getTitle()->equals(Title::newMainPage()) )
+		$bodyAttrs['class'] .= ' mainpage';
+	
+	return true;
+}
+
