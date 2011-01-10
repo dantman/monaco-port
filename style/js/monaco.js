848aeea43079e48db29411a453623b8296aecd0f
Daniel Friesen <pub-github@nadir-seen-fire.com>
2011-01-09 16:05:41 -0800

    Switch from .delegate to .live so we're compatible with the outdated copy of jQuery shipped with MediaWiki 1.16.

--- style/js/monaco.js
+++ style/js/monaco.js
@@ -4,22 +4,19 @@
  * @license Dual-licensed MIT / GPL2+
  */
 
-jQuery(function($) {
-	
-	$('body')
-		.delegate(".hovermenu *", "blur", function(e) {
-			$(this).closest('.hovermenu').removeClass('open');
-		})
-		.delegate(".hovermenu *", "focus", function(e) {
-			$(this).closest('.hovermenu').addClass('open');
-		});
-	
-	$('body')
-		.delegate(".hover-navigation *", "blur", function(e) {
-			$(this).closest(".hover-navigation").find('.menu-item.with-sub-menu.open').removeClass('open');
-		})
-		.delegate(".hover-navigation *", "focus", function(e) {
-			$(this).parents(".menu-item.with-sub-menu").addClass('open');
-		});
-	
-});
+$('.hovermenu *')
+	.live("blur", function(e) {
+		$(this).closest('.hovermenu').removeClass('open');
+	})
+	.live("focus", function(e) {
+		$(this).closest('.hovermenu').addClass('open');
+	});
+
+$(".hover-navigation *")
+	.live("blur", function(e) {
+		$(this).closest(".hover-navigation").find('.menu-item.with-sub-menu.open').removeClass('open');
+	})
+	.live("focus", function(e) {
+		$(this).parents(".menu-item.with-sub-menu").addClass('open');
+	});
+
