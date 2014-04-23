7d8723ca8a4626f162c5b8a2ba8e67dffa352377
James Haley <haleyjd@hotmail.com>
2014-04-22 22:48:02 -0500

    Rewrote monaco.js to avoid deprecated .live()
    Use .on() with PlainObject first parameter instead.

--- style/js/monaco.js
+++ style/js/monaco.js
@@ -1,22 +1,24 @@
 /**
  * Copyright © 2011 - Daniel Friesen
- * @author Daniel friesen
+ * @author Daniel Friesen
+ * @author James Haley
  * @license Dual-licensed MIT / GPL2+
  */
 
-jQuery('.hovermenu *')
-	.live("blur", function(e) {
-		$(this).closest('.hovermenu').removeClass('open');
-	})
-	.live("focus", function(e) {
-		$(this).closest('.hovermenu').addClass('open');
-	});
+$('.hovermenu *').on({
+  blur: function() {
+    $(this).closest('.hovermenu').removeClass('open');
+  },
+  focus: function() {
+    $(this).closest('.hovermenu').addClass('open');
+  }
+});
 
-jQuery(".hover-navigation *")
-	.live("blur", function(e) {
-		$(this).closest(".hover-navigation").find('.menu-item.with-sub-menu.open').removeClass('open');
-	})
-	.live("focus", function(e) {
-		$(this).parents(".menu-item.with-sub-menu").addClass('open');
-	});
-
+$(".hover-navigation *").on({
+  blur: function() {
+    $(this).closest(".hover-navigation").find('.menu-item.with-sub-menu.open').removeClass('open');
+  },
+  focus: function() {
+    $(this).parents(".menu-item.with-sub-menu").addClass('open');
+  }
+});
